55a8d1dfffb1c98dc08362979ca4dcfe46fb6d8e
Eliyas Hossain <eliyashossain700@gmail.com>
2022-06-30 18:02:55 +0600

    MongoDB Connected Successfully

--- index.js
+++ index.js
@@ -4,8 +4,30 @@
  */
 
 const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+//Connect with MongoDB
+
+const uri = `mongodb+srv://${process.env.MY_USER}:${process.env.MY_PASSWORD}@cluster0.wu2yr.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log("DB Connected BRo");
+  client.close();
+});
 
 app.get("/", (req, res) => {
   res.send("Hello! Power Hack Server is Running Smoothly");
