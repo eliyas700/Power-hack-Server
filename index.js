6b9b1c1edde2cf2618e62b2f228236f18776e82c
Eliyas Hossain <eliyashossain700@gmail.com>
2022-06-30 18:34:27 +0600

    MongoDB Set Up Completed

--- index.js
+++ index.js
@@ -15,20 +15,32 @@ app.use(cors());
 app.use(express.json());
 
 //Connect with MongoDB
-
-const uri = `mongodb+srv://${process.env.MY_USER}:${process.env.MY_PASSWORD}@cluster0.wu2yr.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.MY_USER}:${process.env.MY_PASSWORD}@cluster0.6d2m8.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  console.log("DB Connected BRo");
-  client.close();
-});
+// client.connect((err) => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
 
+async function run() {
+  try {
+    await client.connect();
+
+    const usersCollection = client.db("power-hack").collection("users_list");
+    //Get All Users
+    app.get("/user", async (req, res) => {
+      const users = await usersCollection.find().toArray();
+      res.send(users);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("Hello! Power Hack Server is Running Smoothly");
 });
